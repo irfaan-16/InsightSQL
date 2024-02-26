9091a7c36a992ce7e36997024bbd9f7a6092627f
irfaan <irfaank778@gmal.com>
2024-02-26 08:37:47 -0800

    added output tables and loading indicators

--- components/Table.tsx
+++ components/Table.tsx
@@ -4,17 +4,22 @@ const Table = ({ table }) => {
       <thead>
         <tr>
           {table?.columns.map((col) => (
-            <th className="border-2 border-dashed p-2">{col}</th>
+            <th className="border-2 border-dashed p-2" key={col + Date.now()}>
+              {col}
+            </th>
           ))}
         </tr>
       </thead>
       <tbody>
         {table?.output.map((row, idx) => {
           return (
-            <tr>
+            <tr key={idx + Date.now()}>
               {Object.values(row).map((col, idx): any => {
                 return (
-                  <td className="border-2 border-dashed p-2">
+                  <td
+                    className="border-2 border-dashed p-2"
+                    key={(col as string) + idx}
+                  >
                     {col as string}
                   </td>
                 );
