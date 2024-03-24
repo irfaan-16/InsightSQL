d9909e317d33434485fe08aead5b1dcc7cf5a951
irfaan <irfaank778@gmal.com>
2024-03-24 14:58:18 +0530

    fixed connection bug

--- app/question/[id]/page.tsx
+++ app/question/[id]/page.tsx
@@ -1,9 +1,10 @@
 import QuestionDetails from "@/components/QuestionDetails";
 import Question from "@/lib/models/Question";
-
+import connect from "@/lib/database/mongo";
 const page = async ({ params }) => {
   async function getQuestion() {
     "use server";
+    await connect();
     const question = await Question.findById(params.id);
     return question;
   }
